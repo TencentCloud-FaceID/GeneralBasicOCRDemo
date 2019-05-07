dc6e4533c79c5411a95575405a9d399f51a95a9c
v_azchzhang <v_azchzhang@tencent.com>
2019-05-07 11:40:55 +0800

    fix bug

--- miniprogram/pages/coordinate/coordinate.js
+++ miniprogram/pages/coordinate/coordinate.js
@@ -7,12 +7,13 @@ Page({
     json_data: "JSON",
     json: "",
     windowWidth: "",
-    windowWidth_data: ""
+    windowWidth_data: "",
+    height_scale:""
   },
   result() {
     var myThis = this
     wx.redirectTo({
-      url: '../result/result?json=' + myThis.data.json + "&imageurl=" + myThis.data.imageurl + "&width=" + myThis.data.width + "&height=" + myThis.data.height + "&web=coordinate" + "&windowWidth=" + myThis.data.windowWidth_data
+      url: '../result/result?json=' + myThis.data.json + "&imageurl=" + myThis.data.imageurl + "&width=" + myThis.data.width + "&height=" + myThis.data.height + "&web=coordinate" + "&windowWidth=" + myThis.data.windowWidth_data + "&height_scale=" + myThis.data.height_scale
     })
   },
   onLoad: function (options) {
@@ -26,15 +27,18 @@ Page({
       json_data: json_stringify,
       json: options.json,
       windowWidth: options.windowWidth / 2,
-      windowWidth_data: options.windowWidth
+      windowWidth_data: options.windowWidth,
+      height_scale:options.height_scale
     })
     var width_num = parseFloat(options.width)
+    var height_scale_num = parseFloat(options.height_scale)
     const ctx = wx.createCanvasContext('Canvas'); //首页LOGO
     ctx.drawImage(options.imageurl, 0, 0, width_num, 200);
-    ctx.setFontSize(15)
+    ctx.setFontSize(10)
     ctx.setFillStyle('red')
+    ctx.setTextBaseline('middle')
     for (var i = 0; i < json_parse.result.TextDetections.length; i++) {
-      ctx.fillText(json_parse.result.TextDetections[i].DetectedText, json_parse.result.TextDetections[i].Polygon[0].X, json_parse.result.TextDetections[i].Polygon[0].Y)
+      ctx.fillText(json_parse.result.TextDetections[i].DetectedText, json_parse.result.TextDetections[i].Polygon[0].X * height_scale_num, json_parse.result.TextDetections[i].Polygon[0].Y * height_scale_num)
     }
     ctx.draw();
   }
--- miniprogram/pages/result/result.js
+++ miniprogram/pages/result/result.js
@@ -5,19 +5,24 @@ Page({
     width: 200,
     json_data: [],
     json: "",
-    windowWidth:"",
-    windowWidth_data:""
+    windowWidth: "",
+    windowWidth_data: "",
+    height_scale: ""
   },
   coordinate() {
     var myThis = this
     wx.redirectTo({
-      url: '../coordinate/coordinate?json=' + myThis.data.json + "&imageurl=" + myThis.data.imageurl + "&width=" + myThis.data.width + "&height=" + myThis.data.height + "&windowWidth=" + myThis.data.windowWidth_data
+      url: '../coordinate/coordinate?json=' + myThis.data.json + "&imageurl=" + myThis.data.imageurl + "&width=" + myThis.data.width + "&height=" + myThis.data.height + "&windowWidth=" + myThis.data.windowWidth_data + "&height_scale=" + myThis.data.height_scale
     })
   },
   onLoad: function(options) {
     var myThis = this;
     switch (options.web) {
       case "index":
+        wx.showLoading({
+          title: '识别中……',
+          mask: false,
+        })
         var height = options.height
         var width = options.width
         var height_scale = 200 / height
@@ -26,7 +31,8 @@ Page({
           imageurl: options.imageurl,
           width: width_height,
           windowWidth: options.windowWidth / 2,
-          windowWidth_data: options.windowWidth
+          windowWidth_data: options.windowWidth,
+          height_scale: height_scale
         })
         wx.cloud.callFunction({
           name: "GeneralBasicOCR",
@@ -34,14 +40,41 @@ Page({
             base64: options.base64,
           },
           success(cloud_callFunction_res) {
-            var newarray = []
-            for (var i = 0; i < cloud_callFunction_res.result.TextDetections.length; i++) {
-              newarray.push(cloud_callFunction_res.result.TextDetections[i].DetectedText)
+            console.log(cloud_callFunction_res.result.TextDetections.length)
+            if (cloud_callFunction_res.result.TextDetections.length == 0){
+              wx.hideLoading()
+              wx.showModal({
+                title: '提示',
+                content: '未检测到文字，请重试',
+                success(res) {
+                  if (res.confirm) {
+                    wx.redirectTo({
+                      url: '../index/index'
+                    })
+                  } else if (res.cancel) {
+                    wx.redirectTo({
+                      url: '../index/index'
+                    })
+                  }
+                }
+              })
+
+            }else{
+              var newarray = []
+              for (var i = 0; i < cloud_callFunction_res.result.TextDetections.length; i++) {
+                newarray.push(cloud_callFunction_res.result.TextDetections[i].DetectedText)
+              }
+              myThis.setData({
+                json_data: newarray,
+                json: JSON.stringify(cloud_callFunction_res)
+              })
+              wx.hideLoading()
+              wx.showToast({
+                title: '成功',
+                icon: 'success',
+                duration: 200
+              })
             }
-            myThis.setData({
-              json_data: newarray,
-              json: JSON.stringify(cloud_callFunction_res)
-            })
           },
         })
         break;
@@ -57,11 +90,11 @@ Page({
           height: options.height,
           json_data: newarray,
           json: JSON.stringify(json_parse),
-          windowWidth: options.windowWidth/2,
-          windowWidth_data: options.windowWidth
+          windowWidth: options.windowWidth / 2,
+          windowWidth_data: options.windowWidth,
+          height_scale: options.height_scale
         })
         break;
     }
-
   }
 })
